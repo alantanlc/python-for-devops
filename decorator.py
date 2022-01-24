d43ac6e1432a3e2ff93ed1d1d176995655d7cf4b
Alan Tan <alanwuha91@gmail.com>
2022-01-24 21:14:11 +0800

    add timer decorator

--- decorator.py
+++ decorator.py
@@ -1,3 +1,5 @@
+import time
+
 # Python decorators are a special syntax for functions which take other functions as arguments. Python functions are objects, so any function can take a function as an argument. The decorator syntax provides a clean and easy way to do this. The basic format of a decorator is:
 def some_decorator(wrapped_function):
   def wrapper():
@@ -6,17 +8,25 @@ def some_decorator(wrapped_function):
     print('Do something after calling wrapped function')
   return wrapper
   
+def timer(wrapped_function):
+  def wrapper():
+    start = time.time()
+    wrapped_function()
+    end = time.time()
+    print(f'{wrapped_function.__name__} took {end - start} seconds to run')
+  return wrapper
+  
 # You can define a function and pass it as an argument to this function:
 def foobat():
   print('foobat')
   
 f = some_decorator(foobat)
 
-f()
+# f()
 
 # The decorator syntax simplifies this by indicating which function should be wrapped by decorating it with @decorator_name. Here is an example using the decorator syntax with our some_decorator function:
 
-@some_decorator
+@timer
 def batfoo():
   print('batfoo')
   
